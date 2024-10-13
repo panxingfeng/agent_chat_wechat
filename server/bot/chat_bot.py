0e2b2a14aa4c8c8eb100f5e0c5e01e0c85554d17
PanXingFeng <114853016+panxingfeng@users.noreply.github.com>
2024-10-14 03:09:56 +0800

    Update chat_bot.py

--- server/bot/chat_bot.py
+++ server/bot/chat_bot.py
@@ -97,7 +97,6 @@ class Chat_Bot_Chat:
 
     async def run(self, user_name, query, user_id):
         """主运行逻辑，管理历史记录、生成回复，并保存会话记录"""
-        logging.info(f"接收到用户id为：{user_id}，用户名为{user_name}的消息")
         self.manage_history()
 
         # 将用户输入加入历史记录
