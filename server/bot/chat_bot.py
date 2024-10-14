49e3b6c059b4270cfa09a364e97183fc57f1abdb
PanXingFeng <114853016+panxingfeng@users.noreply.github.com>
2024-10-14 13:33:05 +0800

    Update chat_bot.py

--- server/bot/chat_bot.py
+++ server/bot/chat_bot.py
@@ -5,7 +5,8 @@ import os
 from datetime import datetime
 from langchain_openai import ChatOpenAI
 
-from config.config import CHATGPT_DATA, REDIS_DATA
+from config.config import CHATGPT_DATA, REDIS_DATA, OLLAMA_DATA
+from server.client.ollama_client import OllamaClient
 
 # 配置日志记录系统
 logging.basicConfig(level=logging.INFO,
@@ -36,15 +37,23 @@ class Chat_Bot_Chat:
         self.history = []  # 用于存储会话历史记录的列表
 
         # 动态加载OpenAI模型
-        self.model = self.get_openai_model()
+        self.model = self.get_model_client()
 
-    def get_openai_model(self):
-        """根据配置文件中的API密钥、API地址和模型名称加载OpenAI模型"""
-        return ChatOpenAI(
-            api_key=CHATGPT_DATA.get("key"),
-            base_url=CHATGPT_DATA.get("url"),
-            model=CHATGPT_DATA.get("model")
-        )
+    def get_model_client(self):
+        """根据配置文件选择返回的模型"""
+        if OLLAMA_DATA.get("use"):
+            logging.info(f"使用Ollama模型生成回复: {OLLAMA_DATA.get("model")}")
+            return OllamaClient(
+                model_name=OLLAMA_DATA.get("model"),
+                ollama_url=OLLAMA_DATA.get("url")
+            )
+        else:
+            logging.info(f"使用OpenAI模型生成回复: {CHATGPT_DATA.get('model')}")
+            return ChatOpenAI(
+                api_key=CHATGPT_DATA.get("key"),
+                base_url=CHATGPT_DATA.get("url"),
+                model=CHATGPT_DATA.get("model")
+            )
 
     def manage_history(self):
         """管理会话历史记录，从Redis加载历史记录，并限制记录的大小和字符长度"""
@@ -79,9 +88,8 @@ class Chat_Bot_Chat:
             logging.error(f"保存历史记录到Redis时出错: {e}")
 
     def generate_response(self, query):
-        """生成AI回复，使用OpenAI模型"""
+        """生成AI回复"""
         try:
-            logging.info(f"使用OpenAI模型生成回复: {CHATGPT_DATA.get('model')}")
             # 调用invoke时，需要传入适合的消息结构
             messages = [
                 {"role": "system", "content": "你是一个智能助手"},
@@ -92,11 +100,12 @@ class Chat_Bot_Chat:
                 logging.info(f"成功生成回复")
                 return response.content  # 确保返回内容
         except Exception as e:
-            logging.warning(f"OpenAI模型生成回复失败: {e}")
+            logging.warning(f"模型生成回复失败: {e}")
             return "模型生成回复失败，请稍后再试。"
 
     async def run(self, user_name, query, user_id):
         """主运行逻辑，管理历史记录、生成回复，并保存会话记录"""
+        logging.info(f"接收到用户id为：{user_id}，用户名为{user_name}的消息")
         self.manage_history()
 
         # 将用户输入加入历史记录
@@ -107,10 +116,11 @@ class Chat_Bot_Chat:
         # 生成AI回复
         response = self.generate_response(query)
 
-        # 将生成的回复加入历史记录
-        self.history.append({
-            "AI": response,
-        })
+        # # 将生成的回复加入历史记录
+        # self.history.append({
+        #     "AI": response,
+        # })
+        # 可以做保存也可以不做保存
 
         # 保存更新后的历史记录到Redis
         self.save_history_to_redis()
