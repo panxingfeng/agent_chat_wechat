c3a68a6acefaad0357450497fbbb790c71418957
PanXingFeng <114853016+panxingfeng@users.noreply.github.com>
2024-10-16 19:54:15 +0800

    Update chat_bot.py

--- server/bot/chat_bot.py
+++ server/bot/chat_bot.py
@@ -38,6 +38,7 @@ for i in range(device_count):
     handle = pynvml.nvmlDeviceGetHandleByIndex(i)
     mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
 
+
 class ChatBot:
     def __init__(self, user_name, user_id):
         """初始化ChatBot类，设置用户信息和查询，加载OpenAI模型"""
@@ -139,10 +140,15 @@ class ChatBot:
                 history=self.format_history(),
                 query=query,
             )
-            messages = [
-                {"role": "system", "content": instructions},
-                {"role": "user", "content": query}
-            ]
+            if BAICHUAN_DATA.get("use") and BAICHUAN_DATA.get("key") is not None:
+                messages = [
+                    {"role": "user", "content": query}
+                ]
+            else:
+                messages = [
+                    {"role": "system", "content": instructions},
+                    {"role": "user", "content": query}
+                ]
             response = self.model.invoke(messages)
             if response:
                 logging.info(f"成功生成回复")
@@ -151,7 +157,7 @@ class ChatBot:
             logging.warning(f"模型生成回复失败: {e}")
             return "模型生成回复失败，请稍后再试。"
 
-    async def run(self, user_name, query, user_id):
+    async def run(self, user_name, query, user_id, image_path, file_path):
         """主运行逻辑，管理历史记录、生成回复，并保存会话记录"""
         logging.info(f"接收到用户id为：{user_id}，用户名为{user_name}的消息")
         self.manage_history()
