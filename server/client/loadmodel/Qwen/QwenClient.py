05a09ab8109b26b44e403f3aaa933a023b1981f7
PanXingFeng <114853016+panxingfeng@users.noreply.github.com>
2024-10-16 19:26:37 +0800

    Update and rename qwen2_5_0.5b.py to QwenClient.py

--- server/client/loadmodel/Qwen/QwenClient.py
+++ server/client/loadmodel/Qwen/QwenClient.py
@@ -7,17 +7,29 @@ class ResponseWrapper:
     def __init__(self, content):
         self.content = content
 
-class QwenClient_0_5B:
+class QwenClient:
     """
-    调用本地部署的Qwen2.5-0.5B大模型，模型的配置信息在config/config.py中
+    num=1:调用本地部署的Qwen2.5-1.5B大模型，模型的配置信息在config/config.py中
+    示例模板: messages = [
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": message}
+    ]
+    GPU:运行要求4G+显存，速度看显卡的型号
+
+    num=2:调用本地部署的Qwen2.5-0.5B大模型，模型的配置信息在config/config.py中
     示例模板: messages = [
         {"role": "system", "content": prompt},
         {"role": "user", "content": message}
     ]
     GPU:运行要求2G+显存，速度看显卡的型号
     """
-    def __init__(self, model_path="Qwen/Qwen2.5-0.5B-Instruct", cache_dir="../../model"):
+    def __init__(self, num, cache_dir="../../../model"):
+        global model_path
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        if num == 1:
+            model_path = "QwenModel/Qwen2.5-0.5B-Instruct"
+        elif num == 2:
+            model_path = "QwenModel/Qwen2.5-1.5B-Instruct"
         self.tokenizer = AutoTokenizer.from_pretrained(
             model_path,
             trust_remote_code=True,
@@ -64,7 +76,7 @@ class QwenClient_0_5B:
 
 # 测试示例
 if __name__ == "__main__":
-    client = QwenClient_0_5B()
+    client = QwenClient(num=1)
     message = "如何学习好python"
     prompt = "你是一个乐于助人的机器人"
     # 生成并打印模型的回复
