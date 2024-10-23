99095790a64a22da2de203bf3741cbade6a389b5
PanXingFeng <114853016+panxingfeng@users.noreply.github.com>
2024-10-23 23:20:45 +0800

    Update tool.py

--- tools/swarm_tool/tool.py
+++ tools/swarm_tool/tool.py
@@ -1,5 +1,3 @@
-from langchain_community.tools import DuckDuckGoSearchResults
-from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
 from config.templates.data.bot import CODE_BOT_PROMPT_DATA
 from server.client.loadmodel.Ollama.OllamaClient import OllamaClient
 
@@ -15,15 +13,3 @@ def code_gen(query: str, code_type: str) -> str:
 
     response = client.invoke(messages)
     return response
-
-def search_tool(query: str) -> str:
-    """联网搜索工具"""
-    # 初始化 DuckDuckGoSearchAPIWrapper
-    wrapper = DuckDuckGoSearchAPIWrapper(region="de-de", time="d", max_results=2)
-
-    # 创建 DuckDuckGoSearchResults 实例
-    search = DuckDuckGoSearchResults(api_wrapper=wrapper, source="text")
-
-    # 执行搜索
-    result = search.invoke(query)
-    return result
