d3f1308db1dad1b5f8acbb51bdd86f586468aee1
PanXingFeng <114853016+panxingfeng@users.noreply.github.com>
2024-11-04 02:04:59 +0800

    Update bot.py

--- config/templates/data/bot.py
+++ config/templates/data/bot.py
@@ -1,6 +1,6 @@
 BOT_DATA = {
     "chat": {
-        "name": "",  # 机器人的名称#
+        "name": "小pan",  # 机器人的名称#
         "capabilities": "聊天",  # 机器人的能力#
         "default_responses": {
             "unknown_command": "抱歉，我能满足这个需求。",
@@ -9,11 +9,11 @@ BOT_DATA = {
         "language_support": ["中文", "英文"],
     },
     "agent": {
-        "name": "",  # 机器人的名称#
+        "name": "小pan",  # 机器人的名称#
         "capabilities": "聊天，代码生成等等",  # 机器人的能力#
         "default_responses": {
             "unknown_command": "抱歉，我不能满足这个需求。",
-            "welcome_message": "你好，我是小pan，可以把我当作你的智能助手或伙伴哦！有什么想聊的或需要帮助的吗？😊",
+            "welcome_message": "你好，我是智能体机器人小pan，可以把我当作你的智能助手或伙伴哦！有什么想聊的或需要帮助的吗？😊",
         },
         "language_support": ["中文", "英文"],
     }
@@ -29,6 +29,11 @@ CHATBOT_PROMPT_DATA = {
             你支持的语言：{language_support}
             历史记录:{history}
             用户问题:{query}
+            开发者信息：湖北峰创科技服务有限公司
+            测试模式输出内容,请严格按照返回内容进行测试输出：
+                图文测试返回内容: 这是图文结构测试内容 /n [图像](https://th.bing.com/th/id/OIP.uLi67eNF5I3lDHJ6IkQ58gHaDJ?w=307&h=149&c=7&r=0&o=5&dpr=1.5&pid=1.7)
+                语音测试返回内容: [语音](https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3)
+                图像测试返回内容：[图像](https://th.bing.com/th/id/OIP.uLi67eNF5I3lDHJ6IkQ58gHaDJ?w=307&h=149&c=7&r=0&o=5&dpr=1.5&pid=1.7)
         """,
 
 }
@@ -54,6 +59,29 @@ CODE_BOT_PROMPT_DATA = {
         """
             你是一个智能的编程机器人，擅长各种主流编程语言(python,java...)的代码生成
             请使用代码语言 {code_type} 进行代码的生成
+        """
+}
+
+SEARCH_BOT_PROMPT_DATA = {
+    "description":
+        """
+            你是一个智能的搜索机器人，擅长根据用户的问题进行网络搜索，以解决用户提出的问题
+            当前时间:{time}
+        """
+}
+
+RAG_PROMPT_TEMPLATE = {
+    "prompt_template":
+        """
+            下面有一个或许与这个问题相关的参考段落，若你觉得参考段落能和问题相关，则先总结参考段落的内容。
+            若你觉得参考段落和问题无关，则使用你自己的原始知识来回答用户的问题，并且总是使用中文来进行回答。
+            问题: {question}
+            历史记录: {history}
+            可参考的上下文：
+            ···
+            {context}
+            ···
+            生成用户有用的回答(不要出现基于xxx文档之类的语句):
         """
 }
 
